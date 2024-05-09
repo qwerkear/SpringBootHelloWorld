426a57b42f1c5df855f2585c45137d9a68d6e34d
Jamie <qwer.kear@gmail.com>
2024-05-09 11:32:18 -0400

    Create controller, seperate logic, add an endpoint

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @RequestMapping(value = "/cpiData", method = RequestMethod.GET)
+    public String getCPIData() {
+        String cpiApiUrl = "https://api.bls.gov/publicAPI/v2/timeseries/data/CUUR0000SA0L1E";
+        ResponseEntity<String> response = restTemplate.getForEntity(cpiApiUrl, String.class);
+        return response.getBody();
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,17 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping
-	public String helloWorld() {return "Hello World!";}
-
 }
